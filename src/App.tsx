fbe5e550cafb14febbec23d618e597c6f0261bd3
Haidar Ahmad <haidara.ahmad@tikit.ae>
2026-04-21 09:31:57 +0300

    Add react-router-dom for routing and update App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+
 function App() {
   return (
-    <main>
-      <h1>Welcome</h1>
-      <p>Your app is ready.</p>
-    </main>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
